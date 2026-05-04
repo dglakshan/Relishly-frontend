cb863827ed601ebf5c1b09e37e80e7cf44d7ca1a
dglakshan <pasindulakshanun@gmail.com>
2026-05-04 12:06:25 +0530

    SEO added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import AboutUs from "../Components/About";
 import BookTable from "../Components/BookTable";
 import Footer from "../Components/Footer";
@@ -10,50 +11,59 @@ import Testimonial from "../Components/Testimonial";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen grid grid-cols-1 bg-primary-2">
-      {/* NavBar */}
-      <div className="w-full h-fit fixed top-0 left-0 z-100">
-        <NavBar />
-      </div>
+    <>
+      <Helmet>
+        <title>Home | Relishly Restaurant</title>
+        <meta
+          name="description"
+          content="Explore our fresh menu and book your table today."
+        />
+      </Helmet>
+      <div className="w-full min-h-screen grid grid-cols-1 bg-primary-2">
+        {/* NavBar */}
+        <div className="w-full h-fit fixed top-0 left-0 z-100">
+          <NavBar />
+        </div>
 
-      {/* Hero */}
-      <div className="w-full h-fit">
-        <Hero />
-      </div>
+        {/* Hero */}
+        <div className="w-full h-fit">
+          <Hero />
+        </div>
 
-      {/* Offers */}
-      <div className="w-full h-fit">
-        <Offers />
-      </div>
+        {/* Offers */}
+        <div className="w-full h-fit">
+          <Offers />
+        </div>
 
-      {/* About */}
-      <div className="w-full h-fit">
-        <AboutUs />
-      </div>
+        {/* About */}
+        <div className="w-full h-fit">
+          <AboutUs />
+        </div>
 
-      {/* Menu */}
-      <div className="w-full h-fit">
-        <Menu />
-      </div>
+        {/* Menu */}
+        <div className="w-full h-fit">
+          <Menu />
+        </div>
 
-      {/* gallery */}
-      <div className="w-full h-fit">
-        <Gallery />
-      </div>
+        {/* gallery */}
+        <div className="w-full h-fit">
+          <Gallery />
+        </div>
 
-      <div className="w-full h-fit">
-        <BookTable />
-      </div>
+        <div className="w-full h-fit">
+          <BookTable />
+        </div>
 
-      {/* Testimoniols */}
-      <div className="w-full h-fit">
-        <Testimonial />
-      </div>
+        {/* Testimoniols */}
+        <div className="w-full h-fit">
+          <Testimonial />
+        </div>
 
-      {/* Footer */}
-      <div className="w-full h-fit">
-        <Footer />
+        {/* Footer */}
+        <div className="w-full h-fit">
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
